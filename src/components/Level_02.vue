f3606c2baa7b8288a30cc447dba4e1137e3023a8
Колтович Дмитрий <dkoltovich@roox.ru>
2019-01-11 18:22:01 +0300

    Init

---
+++ src/components/Level_02.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+Dfdssf
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Level_02',
+    data () {
+      return {
+
+      }
+    },
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+    watch: {
+
+    }
+  }
+</script>
+
+<!-- Добавьте атрибут "scoped", чтобы ограничить CSS только этим компонентом -->
+<style scoped>
+  h1, h2 {
+    font-weight: bold;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  span {
+    border: black;
+    border-style: solid
+  }
+
+  div {
+    margin-top: 2em;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Level_02 from '@/components/Level_02'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Level_02',
+      component: Level_02
     }
   ]
 })
