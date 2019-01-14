c29f0f5e18ebb8f222348a2b37b6ba15c5f320be
Колтович Дмитрий <steklopod@gmail.com>
2019-01-14 13:47:09 +0300

    Init

---
+++ src/components/Level_05.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="main">
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Level_05',
+    data () {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {}
+
+  }
+</script>
+
+<!-- Добавьте атрибут "scoped", чтобы ограничить CSS только этим компонентом -->
+<style scoped>
+  h1, h2 {
+    font-weight: bold;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  span {
+    border: black;
+    border-style: solid
+  }
+
+  div {
+    margin-top: 2em;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Level_04 from '@/components/Level_04'
+import Level_05 from '@/components/Level_05'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Level_04',
-      component: Level_04
+      name: 'Level_05',
+      component: Level_05
     }
   ]
 })
