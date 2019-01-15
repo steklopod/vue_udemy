8f67d46bc28326be3ebc64b47985bf406476bf9e
Колтович Дмитрий <steklopod@gmail.com>
2019-01-15 16:24:03 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,24 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-user></app-user>
+
+        <app-quote>
+
+        </app-quote>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import User from './components/User.vue'
-
+import Quote from './components/Quote.vue'
   export default {
-    components: {
-      appUser: User
+    components:{
+      appQuote: Quote
     }
   }
 </script>
 
 <style>
-  div.component {
-    border: 1px solid black;
-    padding: 30px;
-  }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>
+      Цитата
+    </p>
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: auto;
+}
+
+</style>
