ff60ad05fcca672448b16ed6fa9456462efa04b9
Колтович Дмитрий <steklopod@gmail.com>
2019-01-15 14:33:25 +0300

    Props validation

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,24 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User name: {{ name}}</p>
+    <p>User name: {{ name }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['name']
+    props: {
+      name: {
+        type: String,
+        default: ' Need Some Name :-( '
+      }
+    },
+    methods: {
+      swithName () {
+        return this.name.split('').reverse().join('')
+      }
+    }
+
   }
 </script>
 
