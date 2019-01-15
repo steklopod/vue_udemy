9e481acd659846b0992245ffac1920e7e7b11b72
Колтович Дмитрий <steklopod@gmail.com>
2019-01-15 17:17:51 +0300

    <component> tag

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,21 @@
     <div class="row">
       <div class="col-xs-12">
 
-        <app-quote>
-          <h2 slot="title">{{quoteTitle}}</h2>
-          <p slot="content">Жить прекрасно</p>
-        </app-quote>
+        <button @click="selectedComponent='appQuote'">Quote</button>
+        <button @click="selectedComponent='appAuthor'">Author</button>
+        <button @click="selectedComponent='appNew'">New</button>
+        <hr>
+        <p>Выбранный компонент: {{selectedComponent}}</p>
+
+        <component :is="selectedComponent">
+          <p>По умолчанию</p>
+        </component>
+        <!--<app-quote>-->
+          <!--<h2 slot="title">{{quoteTitle}}</h2>-->
+          <!--<p slot="content">Жить прекрасно</p>-->
+        <!--</app-quote>-->
+
+        <hr/>
 
       </div>
     </div>
@@ -15,21 +26,32 @@
 
 <script>
   import Quote from './components/Quote.vue'
+  import Author from './components/Author.vue'
+  import New from './components/New.vue'
 
   export default {
     data () {
       return {
-        quoteTitle: ' Цитата №1'
-
+        quoteTitle: ' Цитата №1',
+        selectedComponent: 'appQuote'
       }
     },
 
     components: {
-      appQuote: Quote
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New,
     }
   }
 </script>
 
 <style scoped>
-
+  div {
+    margin: 10px auto;
+    text-align: center;
+  }
+  p{
+    color: orange;
+    font-family: "Comic Sans MS",serif;
+  }
 </style>
