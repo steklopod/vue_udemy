b9c1b94fe61baba66164246fecbf45ea4ff10734
Колтович Дмитрий <steklopod@gmail.com>
2019-01-16 11:18:12 +0300

    Filters done

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highlight', {
-    bind(el, binding, vnode) {
-        // el.style.backgroundColor = 'green';
-        // el.style.backgroundColor = binding.value;
-        var delay = 0;
-        if (binding.modifiers['delayed']) {
-            delay = 3000;
-        }
-        setTimeout(() => {
-            if (binding.arg == 'background') {
-                el.style.backgroundColor = binding.value;
-            } else {
-                el.style.color = binding.value;
-            }
-        }, delay);
-    }
+Vue.filter('to-lowercase', function(value) {
+  return value.toLowerCase();
 });
 
 new Vue({
